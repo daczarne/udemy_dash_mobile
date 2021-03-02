ec5f26f97c66c0ac86aeb7a8e57531e6dc268a77
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-03-01 22:38:14 -0300

    barchart

---
+++ 03_plotly_chart/01_bar_chart.py
@@ -0,0 +1,32 @@
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.offline import init_notebook_mode, plot
+
+df = px.data.gapminder().query("country=='India'")
+df2 = px.data.gapminder().query("country=='United States'")
+
+fig = go.Figure(
+  [
+    go.Bar(
+      x = df["year"],
+      y = df["gdpPercap"],
+      marker_color = "indianred",
+      name = "India"
+    ),
+    go.Bar(
+      x = df2["year"],
+      y = df2["gdpPercap"],
+      marker_color = "blue",
+      name = "US"
+    )
+  ]
+)
+
+fig.update_layout(
+  title = "GDP per capita over the years",
+  xaxis_title = "Years",
+  yaxis_title = "GDP per capita",
+  barmode = "group"
+)
+
+plot(fig)
