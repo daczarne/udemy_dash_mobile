fd9f9114879c97e0a0aa6238b2f2fec8c92d4a28
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-03-02 19:34:41 -0300

    dash table

---
+++ 05_dash_components/03_dash_table.py
@@ -0,0 +1,59 @@
+import dash
+import dash_html_components as html
+import dash_table
+import pandas as pd
+
+# Load the data
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/multiple_y_axis.csv")
+
+data = {
+	"Date": ["July 12th, 2013 - July 25th, 2013", "July 12th, 2013 - July 25th, 2013"],
+	"Election Polling Organization": ["The New York Times", "Pew Research"],
+	"Rep": [10, 15],
+	"Dem": [5,20],
+	"Region": ["Northern New York State to the Southern Appalachian Mountains", "Canada"]
+}
+
+df = pd.DataFrame(data)
+
+# Instanciate the app
+app = dash.Dash()
+
+# Build table comp
+table = dash_table.DataTable(
+	data = df.to_dict("records"),
+	columns = [{"id": c, "name": c} for c in df.columns],
+	fixed_rows = {"headers": True},
+	style_table = {"maxHeight": "450px"},
+	style_header = {
+		"backgroundColor": "rgb(224, 224, 224)",
+		"fontWeight": "bold",
+		"border": "4px solid white"
+	},
+	style_data_conditional = [
+		{
+			"if": {"row_index": "odd"},
+			"backgroundColor": "rgb(224, 224, 224)",
+			"fontSize": "15px"
+		},
+		{
+			"if": {"row_index": "even"},
+			"backgroundColor": "rgb(255, 255, 255)",
+			"fontSize": "15px"
+		}
+	],
+	style_cell = {
+		"textAlign": "center",
+		"border": "4px solid white",
+		"maxWidth": "50px",
+		"whiteSpace": "normal",
+		"textOverflow": "ellipsis"
+	}
+)
+
+# Build app layout
+app.layout = html.Div(id = "parent_div", children = [table])
+
+# Run the app
+if __name__ == "__main__":
+	app.run_server(debug = True)
